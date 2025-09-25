7c0f0f0a275e5be01ac410b35b85cece39cb3455
Pedro Felipe <pedroumtcf@gmail.com>
2025-09-25 12:01:18 -0300

    Olá merda de mundo

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gitexemplo;
 
-/**
- *
- * @author LENOVO
- */
-public class GitExemplo {
-
-    /**
-     * @param args the command line arguments
-     */
+public classGitExemplo {
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
-    
 }
